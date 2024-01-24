3ce8ae4cd48dd973aa30d3aa70c3543fb976a2f0
Dalton Walker <dev@daltonwalker.com>
2024-01-23 23:07:23 -0500

    init2

--- rag_server.py
+++ rag_server.py
@@ -1,7 +1,35 @@
 from langchain_mixtral import Mixtral8x7b
+from typing import Union
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+import uvicorn
+import requests
+import json
+import ast
+import tomllib
+
+app = FastAPI()
+
+
+def llm_response(prompt: str):
+    model = Mixtral8x7b()
+    for chunk in model.stream(prompt):
+        print(chunk.content, end="", flush=True)
+        yield chunk.content
+
+
+@app.get("/chatbot")
+def chatbot(prompt: str):
+    return StreamingResponse(llm_response(prompt), media_type="text/event-stream")
+
+
+def start_server(host: str, port: int) -> None:
+    uvicorn.run(app, host=host, port=port)
 
 
 if __name__ == "__main__":
-    model = Mixtral8x7b()
-    for chunk in model.stream("Hey there!"):
-        print(chunk.content, end="", flush=True)
+    with open("config.toml", "rb") as f:
+        conf = tomllib.load(f)
+        server_conf = conf["api-server"]
+
+    start_server(server_conf["host"], server_conf["port"])
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 
-url = "http://localhost:8000/chatbot?prompt=hello"
+url = "http://localhost:8000/chatbot?prompt=What kind of AI are you?"
 
 with requests.get(url, stream=True) as r:
     for chunk in r.iter_content(1024, decode_unicode=True):
-        print(chunk, end=" ")
+        print(chunk, end="", flush=True)
