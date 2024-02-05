f614b85eb4e5af406e7e087bdc202d98245cc562
lucian <lucian.cap001@gmail.com>
2024-02-04 19:06:45 -0500

    configured llm-server to use s3

--- build/llm-server/start.py
+++ build/llm-server/start.py
@@ -1,10 +1,32 @@
 import toml
 import os
+import boto3
 
 from pathlib import Path
 from loguru import logger as log
 
-if __name__ == "__main__":
+__USE_LOCAL__ = os.environ.get("LOCAL_MODELS", "False") == "True"
+__BUCKET_NAME__ = "nitmre-models"
+__REMOTE_DIR_NAME__ = "llm/"
+
+def downloadDirectoryFroms3(bucketName, remoteDirectoryName):
+    s3_resource = boto3.resource('s3')
+    bucket = s3_resource.Bucket(bucketName) 
+    for obj in bucket.objects.filter(Prefix = remoteDirectoryName):
+        model_path = os.path.dirname(obj.key).replace('llm', 'models')
+        if not os.path.exists(model_path):
+            os.makedirs(model_path, exist_ok=True)
+        bucket.download_file(obj.key, obj.key.replace('llm', 'models')) # save to same path
+
+if __name__ == "__main__":   
+    # Download from S3
+    if not __USE_LOCAL__:
+        log.info(f"Downloading {__REMOTE_DIR_NAME__} from {__BUCKET_NAME__} S3 Bucket...")
+        downloadDirectoryFroms3(__BUCKET_NAME__, __REMOTE_DIR_NAME__)
+        log.info(f'{__REMOTE_DIR_NAME__} downloaded from {__BUCKET_NAME__} S3 Bucket...')
+    else:
+        log.info(f'Skipping S3 download, using local model directory...')
+
     log.info("Starting NITMRE LLM server...")
 
     conf_file = Path("./config.toml").absolute()
